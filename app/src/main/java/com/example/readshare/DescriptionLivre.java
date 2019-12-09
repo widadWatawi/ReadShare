be5de4b705715d0af5a6dd2193695f01fe27e748
Widad <widad.watawi@gmail.com>
2019-12-09 11:16:15 +0100

    description profile et livre

--- app/src/main/java/com/example/readshare/DescriptionLivre.java
+++ app/src/main/java/com/example/readshare/DescriptionLivre.java
@@ -1,4 +1,55 @@
 package com.example.readshare;
 
-public class DescriptionLivre {
+import android.os.Bundle;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class DescriptionLivre  extends AppCompatActivity {
+
+    @BindView(R.id.titre)
+    TextView titre;
+
+    @BindView(R.id.auteur)
+    TextView auteur;
+
+    @BindView(R.id.genre)
+    TextView genre;
+
+    @BindView(R.id.date)
+    TextView date;
+
+    @BindView(R.id.distance)
+    TextView distance;
+
+    @BindView(R.id.description)
+    TextView description;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.description_livre);
+        ButterKnife.bind(this);
+
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+
+        titre.setText("Titre");
+        auteur.setText("Auteur");
+        genre.setText("Genre");
+        date.setText("Date");
+        distance.setText("200m");
+        description.setText("kjhljhkjhkjhlkjlkjmlkjmklmlkmkmlmjlqkjlfjlfjqhlkhflkjqflkjlfkjqlkfjlqkjflqkjlqkjlfjkl");
+
+
+    }
 }
