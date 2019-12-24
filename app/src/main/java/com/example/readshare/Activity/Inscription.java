76e2499a300479427cbdeb166901b94174588a10
mimme <mariamarkiza96@gmail.com>
2019-12-24 10:25:19 +0100

    add livre modifyuser and livre

--- app/src/main/java/com/example/readshare/Activity/Inscription.java
+++ app/src/main/java/com/example/readshare/Activity/Inscription.java
@@ -1,9 +1,11 @@
 package com.example.readshare.Activity;
 
 import android.annotation.SuppressLint;
+import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -43,6 +45,7 @@ public class Inscription extends AppCompatActivity {
             public void onClick(View v){
                 new HttpReqTask().execute();
             }
+            
         });
 
 
--- app/src/main/java/com/example/readshare/Activity/Profile.java
+++ app/src/main/java/com/example/readshare/Activity/Profile.java
@@ -1,12 +1,26 @@
 package com.example.readshare.Activity;
 
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.readshare.Activity.RechercheLivre.MyMenu;
 import com.example.readshare.R;
+import com.example.readshare.ResponseAuth;
+
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -29,14 +43,28 @@ public class Profile extends AppCompatActivity {
     @BindView(R.id.city)
     TextView city;
 
+    @BindView(R.id.edit_user)
+    TextView editBtn;
 
+    int id = 3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.profile);
         ButterKnife.bind(this);
 
+        editBtn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                Intent intent = new Intent(getApplicationContext(), ModifierProfile.class);
+                startActivity(intent);
+            }
+
+        });
+
+
     }
+
+
 
 
     @Override
